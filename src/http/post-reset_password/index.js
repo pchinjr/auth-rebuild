89db388d6bd1de843e0d89390534436f4dd3a4ed
Paul Chin Jr <pchinjr@gmail.com>
2020-10-22 10:31:52 -0400

    password reset

--- src/http/post-reset_password/index.js
+++ src/http/post-reset_password/index.js
@@ -2,19 +2,45 @@
 
 let arc = require('@architect/functions')
 let data = require('@begin/data')
+let bcrypt = require('bcryptjs')
 
 exports.handler = arc.http.async(reset)
 
 async function reset(req) {
-  console.log(req)
+  console.log(req.body)
 
-  //confirm values are the same
+  //confirm values are the same and validate token to get email
+  if (req.body.password === req.body.confirm) {
 
-  //validate password rules
+    //look up email
+    let result = await data.get({
+      table: 'tokens',
+      key: req.body.token
+    })
+    let email = result.email
 
-  // select user object
+    // save the new password to the account record
+    let salt = bcrypt.genSaltSync(10)
+    let hash = bcrypt.hashSync(req.body.password, salt)
 
-  // write new password to db (optional send email)
-  req.body.
-  return { ok:true}
+    await data.set({
+      table: 'accounts',
+      key: email,
+      password: hash
+    })
+
+    return {
+      session: {
+        account: {
+          email: req.body.email
+        }
+      },
+      location: '/admin'
+    }
+  } else {
+    return {
+      location: '/?password=nomatch'
+    }
+  }
+
 }
